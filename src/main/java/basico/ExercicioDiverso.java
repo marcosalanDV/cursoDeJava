6b84475055a11dfe4d179ca0c941b2ca741758f0
Marcos Alan <marcos38alan@gmail.com>
2023-08-16 18:08:07 -0300

    atualizando projeto

---
+++ src/main/java/basico/ExercicioDiverso.java
@@ -0,0 +1,20 @@
+package basico;
+
+public class ExercicioDiverso {
+
+    public static void main(String[] args) {
+        String nome = "Marcos";
+        int idade = 45;
+        boolean estaTrabalhando = true;
+        double ID = 3_455.0;
+        float salario = 1_255.55F;
+        char status = 'a';
+
+        System.out.println("bem vindo " + nome);
+        System.out.println("sua idade é " + idade);
+        System.out.println("Estou trabalhando no momento " + estaTrabalhando);
+        System.out.println("meu código de identificação é " + ID);
+        System.out.println("Meu salário é: " + salario + "reais");
+        System.out.println("Você está ativo no momento? " + status);
+    }
+}
